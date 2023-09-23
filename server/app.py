101f420e2b6b1cf09ad305bf1c62e7930029af4a
kipngenohaaron <kipngenohaaron@gmail.com>
2023-09-23 22:24:36 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
 
     def get(self):
@@ -37,19 +36,37 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+            plant.is_in_stock = data.get('is_in_stock', plant.is_in_stock)
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)  # Return an empty response for DELETE
+        else:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,6 +11,15 @@ class Plant(db.Model, SerializerMixin):
     image = db.Column(db.String)
     price = db.Column(db.Float)
     is_in_stock = db.Column(db.Boolean)
+    
+    #  def serialize(self):
+    #     return {
+    #         'id': self.id,
+    #         'name': self.name,
+    #         'image': self.image,
+    #         'price': self.price,
+    #         'is_in_stock': self.is_in_stock
+    #     }
 
     def __repr__(self):
         return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
